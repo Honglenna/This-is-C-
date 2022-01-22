d151fd9f87f60c26ca5665dc7ecad8f74d61b93b
HongSW <hsw5715@gmail.com>
2022-01-23 02:37:41 +0900

    chapter 2 practice solution add

---
+++ chapter 2/test1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace test1 // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("여러분, 안녕하세요?");
+            Console.WriteLine("반감습니다!");
+        }
+    }
+}
---
+++ chapter 2/test2/Program.cs
@@ -0,0 +1,11 @@
+﻿// 1번 : System namespace 추가
+// 2번 : Console 클래스 명시
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
