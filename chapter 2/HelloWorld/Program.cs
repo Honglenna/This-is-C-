9720428916182a82a6ac5dbe87b3240958ca89cf
HongSW <hsw5715@gmail.com>
2022-01-22 23:50:25 +0900

    Add repository

---
+++ chapter 2/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace HelloWold
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("사용법 : Hello.exe 홍석우");
+            return;
+        }
+
+        WriteLine("Hello, {0}!", args[0]);
+    }
+    }
+}
+
