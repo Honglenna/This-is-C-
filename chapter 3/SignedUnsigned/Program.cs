5c74987ac7096cd9e0358e0da77dca6c9a897724
HongSW <hsw5715@gmail.com>
2022-01-23 20:19:22 +0900

    chapter 3 Signed Unsigned example

---
+++ chapter 3/SignedUnsigned/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SignedUnsigned
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 255;
+            sbyte b = (sbyte)a;
+
+            System.Console.WriteLine(a);
+            System.Console.WriteLine(b);
+        }
+    }
+}
