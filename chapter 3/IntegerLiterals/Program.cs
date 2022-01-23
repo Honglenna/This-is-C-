bc617eefe1da48ece6d1b0cdbf7062b078d2a662
HongSW <hsw5715@gmail.com>
2022-01-23 19:51:35 +0900

    chapter 3 Binary, Decimal, Hex Literals

---
+++ chapter 3/IntegerLiterals/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IntegerLiterals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 240; // 10진수 리터럴
+            System.Console.WriteLine($"a={a}");
+
+            byte b = 0b1111_0000; // 2진수 리터럴
+            System.Console.WriteLine($"b={b}");
+
+            byte c = 0xf0; // 16진수 리터럴
+            System.Console.WriteLine($"c={c}");
+
+            uint d = 0x1234_abcd; // 16진수 리터럴
+            System.Console.WriteLine($"d={d}");
+        }
+    }
+}
