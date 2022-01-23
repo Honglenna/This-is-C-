7a8b28345c44d9281c1af9285b69df4a903046fc
HongSW <hsw5715@gmail.com>
2022-01-23 19:00:46 +0900

    chapter 3 Integer Type example add

---
+++ chapter 3/IntegerTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IntegerTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte a = -10;
+            byte b = 40;
+
+            Console.WriteLine($"a={a}, b={b}");
+
+            short c = -30000;
+            ushort d = 60000;
+
+            Console.WriteLine($"c={c}, d={d}");
+
+            // 큰 자리수의 정수 리터럴을 타이핑 할 때 자릿수 구분자를 이용하면 편리
+            int e = -10_000_000; // 0이 7개
+            uint f = 300_000_000; // 0이 8개
+
+            Console.WriteLine($"e={e}, f={f}");
+            
+            long g = -500_000_000_000; // 0이 11개
+            ulong h = 2_000_000_000_000_000_000; // 0이 18개
+
+            Console.WriteLine($"g={g}, h={h}");
+        }
+    }
+}
