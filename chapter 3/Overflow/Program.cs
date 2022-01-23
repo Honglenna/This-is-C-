29b19017ce6c4cdf0454380fb6a7123630a876c0
HongSW <hsw5715@gmail.com>
2022-01-23 21:40:01 +0900

    chapter 3 vitamin quiz 3-2 solution

---
+++ chapter 3/Overflow/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint a = uint.MaxValue; // uint 의 최대값 : 4,294,967,295
+
+            // vitamin quiz 3-2
+            // int a = int.MaxValue;
+            // int의 최소값인 -2147483648이 된다.
+
+
+            System.Console.WriteLine(a);
+
+            a = a + 1;
+
+            System.Console.WriteLine(a);
+
+        }
+    }
+}
+
