e48eab84e0bc672d5bb2c66cb02eef2b368c5769
HongSW <hsw5715@gmail.com>
2022-02-14 21:52:11 +0900

    chapter3 Object

---
+++ chapter 3/Object/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object a = 123;
+            object b = 3.1415926535897932238462643383279m;
+            object c = true;
+            object d = "안녕하세요";
+
+            System.Console.WriteLine(a);
+            System.Console.WriteLine(b);
+            System.Console.WriteLine(c);
+            System.Console.WriteLine(d);
+        }
+    }
+}
