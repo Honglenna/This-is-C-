bcce1f3e693b0afe6613176cd778449a3bb041e1
HongSW <hsw5715@gmail.com>
2022-02-14 21:15:17 +0900

    chapter3 String

---
+++ chapter 3/String/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "안녕하세요?";
+            string b = "홍석우 입니다.";
+
+            System.Console.WriteLine(a);
+            System.Console.WriteLine(b);
+        }
+    }
+}
