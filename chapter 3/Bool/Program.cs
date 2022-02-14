26ed2e02f75cd55d811dff5fc4982d9916ce2212
HongSW <hsw5715@gmail.com>
2022-02-14 21:25:25 +0900

    chapter 3 Bool

---
+++ chapter 3/Bool/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            bool b = false;
+
+            System.Console.WriteLine(a);
+            System.Console.WriteLine(b);
+        }
+    }
+}
