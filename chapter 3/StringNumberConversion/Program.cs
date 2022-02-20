96f49a4844dcfcb10f88ffbecfce21fbed46391a
HongSW <hsw5715@gmail.com>
2022-02-20 18:29:50 +0900

    chapter 3 String number conversion

---
+++ chapter 3/StringNumberConversion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StringNumberConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            string b = a.ToString();
+            System.Console.WriteLine(b);
+
+            float c = 3.14f;
+            string d = c.ToString();
+            System.Console.WriteLine(d);
+
+            string e = "123456";
+            int f = Convert.ToInt32(e);
+            System.Console.WriteLine(f);
+
+            string g = "1.2345";
+            float h = float.Parse(g);
+            System.Console.WriteLine(h);
+
+        }
+    }
+}
+
