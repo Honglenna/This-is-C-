836551909fafe683e249fbcfc75531669f66f90a
HongSW <hsw5715@gmail.com>
2022-02-20 19:47:39 +0900

    chapter 3 Nullable

---
+++ chapter 3/Nullable/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a = null;
+
+            System.Console.WriteLine(a.HasValue);
+            System.Console.WriteLine(a != null);
+
+            a = 3;
+            System.Console.WriteLine(a.HasValue);
+            System.Console.WriteLine(a != null);
+            System.Console.WriteLine(a.Value);
+
+        }
+    }
+}
+
