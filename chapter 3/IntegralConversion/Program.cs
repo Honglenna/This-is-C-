1ccd7758f96653dda62d56776548f14a7fb75d7f
HongSW <hsw5715@gmail.com>
2022-02-20 17:29:18 +0900

    chapter3 IntegralConversion

---
+++ chapter 3/IntegralConversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IntegralConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte a = 127;
+            System.Console.WriteLine(a);
+
+            int b = (int)a;
+            System.Console.WriteLine(b);
+
+            int x = 128; // sbyte의 최대값 127보다 1 큰 수
+            System.Console.WriteLine(x);
+
+            sbyte y = (sbyte)x; // overflow
+            System.Console.WriteLine(y);
+        }
+    }
+}
