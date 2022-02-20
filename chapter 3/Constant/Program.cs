bfdac39c53255deed3c7c24431790098395bda75
HongSW <hsw5715@gmail.com>
2022-02-20 18:57:15 +0900

    chapter 3 Constant

---
+++ chapter 3/Constant/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX_INT = 2147483647;
+            const int MIN_INT = -2147483647;
+
+            System.Console.WriteLine(MAX_INT);
+            System.Console.WriteLine(MIN_INT);
+        }
+    }
+}
