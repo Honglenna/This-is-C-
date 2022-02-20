011f3b76b2537de15ceaa72290850ce7e39110bc
HongSW <hsw5715@gmail.com>
2022-02-20 19:13:16 +0900

    chapter 3 Enum

---
+++ chapter 3/Enum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        enum DialogResult { YES, NO, CANCEL, CONFIRM, OK }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine((int)DialogResult.YES);
+            System.Console.WriteLine((int)DialogResult.NO);
+            System.Console.WriteLine((int)DialogResult.CANCEL);
+            System.Console.WriteLine((int)DialogResult.CONFIRM);
+            System.Console.WriteLine((int)DialogResult.OK);
+        }
+
+
+    }
+}
+
