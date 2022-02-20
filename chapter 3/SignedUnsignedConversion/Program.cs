4f6332ea1dcc2d77cfaff049f3528c514815b095
HongSW <hsw5715@gmail.com>
2022-02-20 18:03:39 +0900

    chapter 3 Signed Unsigned Conversion

---
+++ chapter 3/SignedUnsignedConversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SignedUnsignedConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 500;
+            System.Console.WriteLine(a);
+
+            uint b = (uint)a;
+            System.Console.WriteLine(b);
+
+            int x = -30;
+            System.Console.WriteLine(x);
+
+            uint y = (uint)x; // Underflow
+            System.Console.WriteLine(y);
+        }
+    }
+}
+
