f2c81dc58a56fbb404d659c68341d0bca310abcd
HongSW <hsw5715@gmail.com>
2022-02-20 19:21:44 +0900

    chapter 3 Enum2

---
+++ chapter 3/Enum2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Enum2
+{
+    class Program
+    {
+        enum DialogResult { YES, NO, CANCEL, CONFIRM, OK }
+
+        static void Main(string[] args)
+        {
+            DialogResult result = DialogResult.YES;
+
+            System.Console.WriteLine(result == DialogResult.YES);
+            System.Console.WriteLine(result == DialogResult.NO);
+            System.Console.WriteLine(result == DialogResult.CANCEL);
+            System.Console.WriteLine(result == DialogResult.CONFIRM);
+            System.Console.WriteLine(result == DialogResult.OK);
+        }
+    }
+}
+
