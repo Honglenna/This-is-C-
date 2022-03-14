0b429e52998cb83b915349c6a30bd525d51b33f6
HongSW <hsw5715@gmail.com>
2022-03-14 22:09:36 +0900

    chapter3 Using Var

---
+++ chapter 3/UsingVar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UsingVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 20;
+            System.Console.WriteLine("Type: {0}, Value:{1}", a.GetType(), a);
+
+            var b = 3.1414213;
+            System.Console.WriteLine("Type: {0}, Value:{1}", b.GetType(), b);
+
+            var c = "Hello World";
+            System.Console.WriteLine("Type: {0}, Value:{1}", c.GetType(), c);
+
+            var d = new int[] {10, 20, 30};
+            System.Console.Write("Type: {0}, Value:", d.GetType());
+            foreach (var e in d)
+                System.Console.Write("{0} ", e);
+            
+            System.Console.WriteLine();
+
+        }
+    }
+}
+
