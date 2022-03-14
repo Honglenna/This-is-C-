7d7809053f1f8d1a8e56f5f2749f9c13bf96f183
HongSW <hsw5715@gmail.com>
2022-03-14 22:38:08 +0900

    chapter 3 CTS

---
+++ chapter 3/CTS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Int32 a = 123;
+             int b = 456;
+
+             System.Console.WriteLine("a type:{0}, value:{1}", a.GetType().ToString(), a);
+             System.Console.WriteLine("b type:{0}, value:{1}", b.GetType().ToString(), b);
+
+            String c = "abc";
+            string d = "def";
+
+            System.Console.WriteLine("c type:{0}, value:{1}", c.GetType().ToString(), c);
+            System.Console.WriteLine("d type:{0}, value:{1}", d.GetType().ToString(), d);
+        }
+    }
+}
