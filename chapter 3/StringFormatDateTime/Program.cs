3b7f01963d5d37858508928a0aa8b8d9041fe191
HongSW <hsw5715@gmail.com>
2022-05-17 20:39:51 +0900

    chapter3 StringFormatDateTime

---
+++ chapter 3/StringFormatDateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using static System.Console;
+
+namespace StringFormatDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 11, 3, 23, 18, 22);
+
+            WriteLine("12시간 형식 : {0:yyyy-MM-dd tt hh:mm:ss (ddd)}", dt);
+            WriteLine("24시간 형식 : {0:yyyy-MM-dd HH:mm:ss (dddd)}", dt);
+
+            CultureInfo ciKo = new CultureInfo("ko-KR");
+            WriteLine();
+            WriteLine(dt.ToString("yyyy-MM"));
+        }
+    }
+}
+
