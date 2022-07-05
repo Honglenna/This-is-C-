29b7c35254839909706d671f4b4407ebe73090b9
Honglenna <hsw5715@gmail.com>
2022-07-05 21:39:00 +0900

    [chapter 4] Arithmatic operators

--- chapter 4/ArithmaticOperators/Program.cs
+++ chapter 4/ArithmaticOperators/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ArithmaticOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 111 + 222;
+            System.Console.WriteLine($"a : {a}");
+
+            int b = a - 100;
+            System.Console.WriteLine($"b : {b}");
+
+            int c = b * 10;
+            System.Console.WriteLine($"c : {c}");
+
+            double d = c / 6.3;
+            System.Console.WriteLine($"d : {d}");
+
+            System.Console.WriteLine($"22 / 7 = {22 / 7}({22 % 7})");
+        }
+    }
+}
+
+
