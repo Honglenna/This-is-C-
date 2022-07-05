2d076085e5d524511f5a34ddfcea10cd86da50f0
Honglenna <hsw5715@gmail.com>
2022-07-05 22:13:56 +0900

    [chapter 4] String concatenate

---
+++ chapter 4/StringConcatenate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringConcatenate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "123" + "456";
+            System.Console.WriteLine(result);
+
+            result = "Hello" + " " + "World!";
+            System.Console.WriteLine(result);
+        }
+    }
+}
+    
+
+
