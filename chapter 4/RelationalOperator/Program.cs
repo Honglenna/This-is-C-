38991fadb813514a03c4bd4c7369384e36c40f3f
Honglenna <hsw5715@gmail.com>
2022-07-05 22:27:29 +0900

    [chapter 4] Relational operator

---
+++ chapter 4/RelationalOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RelationalOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine($"3 > 4 : {3 > 4}");
+            System.Console.WriteLine($"3 >= 4 : {3 >= 4}");
+            System.Console.WriteLine($"3 < 4 : {3 < 4}");
+            System.Console.WriteLine($"3 <= 4 : {3 <= 4}");
+            System.Console.WriteLine($"3 == 4 : {3 == 4}");
+            System.Console.WriteLine($"3 != 4 : {3 != 4}");
+        }
+    }
+}
