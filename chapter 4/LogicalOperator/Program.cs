43b470f05929d3550f3722bf161656f9bb9c9a95
Honglenna <hsw5715@gmail.com>
2022-07-08 18:49:45 +0900

    [chapter 4] Logical operator

---
+++ chapter 4/LogicalOperator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LogicalOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Testing && .... ");
+            System.Console.WriteLine($"1 > 0 && 4 < 5 : {1 > 0 && 4 < 5}");
+            System.Console.WriteLine($"1 > 0 && 4 > 5 : {1 > 0 && 4 > 5}");
+            System.Console.WriteLine($"1 == 0 && 4 > 5 : {1 == 0 && 4 > 5}");
+            System.Console.WriteLine($"1 == 0 && 4 < 5 : {1 == 0 && 4 < 5}");
+
+            System.Console.WriteLine("\nTesting || .... ");
+            System.Console.WriteLine($"1 > 0 || 4 < 5 : {1 > 0 || 4 < 5}");
+            System.Console.WriteLine($"1 > 0 || 4 > 5 : {1 > 0 || 4 > 5}");
+            System.Console.WriteLine($"1 == 0 || 4 > 5 : {1 == 0 || 4 > 5}");
+            System.Console.WriteLine($"1 == 0 || 4 < 5 : {1 == 0 || 4 < 5}");
+
+            System.Console.WriteLine("\nTesting ! .... ");
+            System.Console.WriteLine($"!True : {!true}");
+            System.Console.WriteLine(($"!False : {!false}"));
+
+        }
+
+
+    }
+}
+
