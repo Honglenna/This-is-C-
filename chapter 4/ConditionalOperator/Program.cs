70ff4e563ea7cbeed250a77e13a11ca0c4dc7d33
Honglenna <hsw5715@gmail.com>
2022-07-08 19:32:45 +0900

    [chapter 4] Conditional operator

---
+++ chapter 4/ConditionalOperator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConditionalOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = (10 % 2) == 0 ? "짝수" : "홀수";
+
+            System.Console.WriteLine(result);
+        }
+    }
+}
+
