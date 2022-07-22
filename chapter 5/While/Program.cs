6dd01cd93ec8e71807d1d0ef5cb3d88172a1b206
Honglenna <hsw5715@gmail.com>
2022-07-22 22:57:29 +0900

    [chapter 5] While

---
+++ chapter 5/While/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            while (i > 0)
+            {
+                System.Console.WriteLine($"i : {i--}");
+            }
+        }
+    }
+}
+
