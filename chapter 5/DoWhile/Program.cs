a6cf637af6ce023a560dfa1f223d56fa44999f72
Honglenna <hsw5715@gmail.com>
2022-07-22 23:19:37 +0900

    [chapter 5] Do While

---
+++ chapter 5/DoWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            do
+            {
+                System.Console.WriteLine($"a) i : {i--}");
+            }
+            while (i > 0);
+
+            do
+            {
+                System.Console.WriteLine($"b) i : {i--}");
+            }
+            while (i > 0);
+        }
+    }
+}
+
