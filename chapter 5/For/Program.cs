4e251cbe6c7ef251aa8351cf9e4dd27515c04291
Honglenna <hsw5715@gmail.com>
2022-07-25 23:39:50 +0900

    [chapter 5] For

---
+++ chapter 5/For/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                System.Console.WriteLine(i);
+            }
+        }
+    }
+}
+
