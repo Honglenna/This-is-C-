eff980e2a9f0c4d41bba2cdef45e81563d65a231
Honglenna <hsw5715@gmail.com>
2022-07-26 23:39:53 +0900

    [chapter 5] Infinite While

---
+++ chapter 5/InfiniteWhile/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InfiniteWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (true)
+                System.Console.WriteLine(i++);
+        }
+    }
+}
+
+
