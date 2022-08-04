cf6a9666524d56c91c5a6032fb74b8e96b32958d
Honglenna <hsw5715@gmail.com>
2022-08-04 22:12:25 +0900

    [chapter 5] Break

---
+++ chapter 5/Break/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("계속할까요? (Y/N) : ");
+                string? answer = Console.ReadLine();
+
+                if (answer == "N")
+                    break;
+            }
+        }
+    }
+}
+
