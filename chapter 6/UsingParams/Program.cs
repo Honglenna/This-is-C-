2f589fbe0537e81ceda42bcf2ed10ea57d0105c4
Honglenna <hsw5715@gmail.com>
2023-07-27 22:49:30 +0900

    [Chapter 6] UsingParams

---
+++ chapter 6/UsingParams/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UsingParams
+{
+    class Program
+    {
+        static int Sum(params int[] args)
+        {
+            System.Console.Write("Summing......");
+
+            int sum = 0;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (i > 0)
+                    System.Console.Write(", ");
+
+                System.Console.Write(args[i]);
+                sum += args[i];
+            }
+            System.Console.WriteLine();
+
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int sum = Sum(3, 4, 5, 6, 7, 8, 9);
+
+            System.Console.WriteLine($"Sum : {sum}");
+        }
+    }
+}
