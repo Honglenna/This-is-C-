e142db1a5fda1916fb84e32e4329b03002ad4d07
Honglenna <hsw5715@gmail.com>
2023-08-01 22:28:21 +0900

    [chapter 7] BasicClass

---
+++ chapter 7/BasicClass/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BasicClass
+{
+    class Cat
+    {
+        public string Name;
+        public string Color;
+
+        public void Meow()
+        {
+            System.Console.WriteLine($"{Name} : Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat();
+            kitty.Color = "White";
+            kitty.Name = "Kitty";
+            kitty.Meow();
+            System.Console.WriteLine($"{kitty.Name} : {kitty.Color}");
+
+             Cat nero = new Cat();
+            nero.Color = "Black";
+            nero.Name = "Nero";
+            nero.Meow();
+            System.Console.WriteLine($"{nero.Name} : {nero.Color}");
+        }
+    }
+}
